4dfddfc74af9e25cdaaa7416c8b8402e024b591d
baberparweez <baberparweez@gmail.com>
2023-06-27 16:43:50 -0300

    updates 1.4

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,14 +1,19 @@
 <footer class="footer">
-	<div class="half">
-		@php $site_logo = get_field('site_logo', 'options') @endphp
-	
-		@if ($site_logo)
-			<a class="footer-logo" href="{{ home_url('/') }}">
-				<img class="logo-img" src="{!! esc_url($site_logo['url']) !!}" alt="{!! esc_attr($site_logo['alt']) !!}" />
-			</a>
-		@endif
-	</div>
-	<div class="half">
+	<div class="wide-container">
+		<div class="half">
+			@php $site_logo = get_field('site_logo', 'options') @endphp
 		
+			@if ($site_logo)
+				<a class="footer-logo" href="{{ home_url('/') }}">
+					<img class="logo-img" src="{!! esc_url($site_logo['url']) !!}" alt="{!! esc_attr($site_logo['alt']) !!}" />
+				</a>
+			@endif
+
+			{!! __('© 2023 Level Design Ltd', 'leveldesignltd') !!}
+		</div>
+		<div class="half">
+			{!! __('Privacy Policy', 'leveldesignltd') !!}
+			{!! __('Terms & Conditions', 'leveldesignltd') !!}
+		</div>
 	</div>
 </footer>
