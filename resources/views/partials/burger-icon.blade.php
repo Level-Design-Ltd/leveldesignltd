4bf09dd19bdc9ccc4a4eccc3fbf0bedb791e320c
baberparweez <baberparweez@gmail.com>
2023-06-27 00:03:18 -0300

    updates 1.2

--- resources/views/partials/burger-icon.blade.php
+++ resources/views/partials/burger-icon.blade.php
@@ -1,5 +1,9 @@
+@php
+	$burgerColour = get_field('burger_menu_colour', get_the_ID());
+@endphp
+
 <svg class="burger" width="30" height="16" viewBox="0 0 32 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path class="line" d="M1 1H31" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
-	<path class="line" d="M1 9H31" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
-	<path class="line" d="M1 17H31" stroke="#fff" stroke-width="2" stroke-linecap="round"/>
+	<path class="line" d="M1 1H31" stroke="{{ $burgerColour === 'dark' ? '#000' : '#fff' }}" stroke-width="2" stroke-linecap="round"/>
+	<path class="line" d="M1 9H31" stroke="{{ $burgerColour === 'dark' ? '#000' : '#fff' }}" stroke-width="2" stroke-linecap="round"/>
+	<path class="line" d="M1 17H31" stroke="{{ $burgerColour === 'dark' ? '#000' : '#fff' }}" stroke-width="2" stroke-linecap="round"/>
 </svg>
