983c0710a405c5df8341f3848c0e0613c4b83463
baberparweez <baberparweez@gmail.com>
2023-06-30 19:24:06 -0300

    action 1.1

--- app/Actions/wpcf7.php
+++ app/Actions/wpcf7.php
@@ -5,7 +5,7 @@ add_action('wp_footer', function() {
 	<script type="text/javascript">
 		document.addEventListener( 'wpcf7mailsent', function( event ) {
 			if ('236' == event.detail.contactFormId) {
-				document.getElementById('wpcf7-f236-p2-o1').style.display = 'none';
+				jQuery('#wpcf7-f236-p2-o1 .wpcf7-form-control').slideUp('slow'); // hides the form fields
 			}
 		}, false );
 	</script>
